2c15c7b62e917f0236c6e746de7c325cb18f9a34
Taiki <tyoshikawa1106@gmail.com>
2015-09-01 20:50:13 +0900

    add event participation/event cancel

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
   root 'welcome#index'
   get '/auth/:provider/callback' => 'sessions#create'
   get '/logout' => 'sessions#destroy', as: :logout
+
+  resources :events do
+    resources :tickets
+  end
 end

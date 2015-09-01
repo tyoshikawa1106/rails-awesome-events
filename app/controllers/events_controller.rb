68b91782ba24eae90fb14fb8f029073369c17429
Taiki <tyoshikawa1106@gmail.com>
2015-09-01 17:00:55 +0900

    add show/new/edit/delete

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,47 @@
 class EventsController < ApplicationController
+  before_action :authenticate, except: :show
+
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  def new
+    @event = current_user.created_events.build
+  end
+
+  def create
+    @event = current_user.created_events.build(event_params)
+    if @event.save
+      redirect_to @event, notice: '作成しました'
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @event = current_user.created_events.find(params[:id])
+  end
+
+  def update
+    @event = current_user.created_events.find(params[:id])
+    if @event.update(event_params)
+      redirect_to @event, notice: '更新しました'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @event = current_user.created_events.find(params[:id])
+    @event.destroy!
+    redirect_to root_path, notice: '削除しました'
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(
+      :name, :place, :content, :start_time, :end_time
+    )
+  end
 end

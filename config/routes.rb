003d4ffc5ab3b8861c4bd3cfc5f4593536378573
Taiki <tyoshikawa1106@gmail.com>
2015-09-01 21:17:26 +0900

    add retire

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
-  resources :events
-  root 'welcome#index'
+  root to: 'welcome#index'
   get '/auth/:provider/callback' => 'sessions#create'
   get '/logout' => 'sessions#destroy', as: :logout
+
+  resource :user do
+    get 'retire'
+  end
 
   resources :events do
     resources :tickets
   end
+  
 end

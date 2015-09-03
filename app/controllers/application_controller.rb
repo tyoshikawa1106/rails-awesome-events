72967dbc4c994a3c54cb33a913dd2aa09ab28771
Taiki <tyoshikawa1106@gmail.com>
2015-09-03 21:34:59 +0900

    commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
 class ApplicationController < ActionController::Base
+  after_action :allow_iframe
+
   protect_from_forgery with: :exception
 
   helper_method :current_user, :logged_in?
 
-  #rescue_from Exception, with: :error500
+  rescue_from Exception, with: :error500
   rescue_from ActiveRecord::RecordNotFound, ActionController::RoutingError, with: :error404
 
   def error404
@@ -30,5 +32,9 @@ class ApplicationController < ActionController::Base
   def authenticate
     return if logged_in?
     redirect_to root_path, alert: 'ログインしてください'
+  end
+
+  def allow_iframe
+    response.headers.except! 'X-Frame-Options'
   end
 end
